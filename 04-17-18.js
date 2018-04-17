5faf3cfe1f057765726587e7f6d7bbe4b4baa792
Sara Brandes <smbrandes17@gmail.com>
2018-04-17 12:21:38 -0700

    Init & Day 1

---
+++ 04-17-18.js
@@ -0,0 +1,15 @@
+/*
+ * Interview Questions (Part VIII)
+ * Chapter 2: Linked Lists
+ * Problem 2.3
+ * 
+ * 
+ * Implement an algorithm to delete a node in the middle of
+ * a singly linked-list, given only access to that node.
+ * 
+ * Example:
+ * Input: Node C from linked list a -> b -> c -> d -> e
+ * Result: Nothing is returned, but new linked list is a -> b -> d -> e
+ *
+*/
+
---
+++ README.md
@@ -0,0 +1,4 @@
+Welcome to HRSF91's interview coding practice prompts.
+
+Make sure you fork the repo and only make pull requests to your branch! 
+Each prompt lists the chapter in CTCI it references / comes from.
